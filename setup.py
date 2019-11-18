788f1d09615b37763b504233dc16c933c709314e
David Cuthbert <dacut@kanga.org>
2019-11-18 11:16:05 -0800

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from __future__ import absolute_import, division, print_function
+from os.path import dirname
 from sys import version_info
 from setuptools import setup, Command
 
@@ -11,9 +12,12 @@ setup_requires=[]
 if version_info[0] > 3 or (version_info[0] == 3 and version_info[1] >= 5):
     setup_requires.append("Sphinx>=1.3")
 
+with open(dirname(__file__) + "/README.rst", "r") as fd:
+    readme = fd.read()
+
 setup(
     name="awssig",
-    version="0.3.0",
+    version="0.3.1",
     packages=['awssig'],
     install_requires=["six>=1.0"],
     setup_requires=setup_requires,
@@ -22,6 +26,8 @@ setup(
     author="David Cuthbert",
     author_email="dacut@kanga.org",
     description="AWS signature verification routines",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
     license="Apache 2.0",
     url="https://github.com/dacut/python-aws-sig",
     classifiers=[
